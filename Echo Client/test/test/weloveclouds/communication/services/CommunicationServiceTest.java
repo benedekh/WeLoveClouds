cb72d60c30e3a47e8e21d5003ba00dcf854729b1
Benoit Anctil-Robitaille <benoit.robitaille@hotmail.ca>
2016-10-23 14:56:04 +0200

    + Added base test clas for communication services, rearragned packaging name added mockito jar

--- Echo Client/test/test/weloveclouds/communication/services/CommunicationServiceTest.java
+++ Echo Client/test/test/weloveclouds/communication/services/CommunicationServiceTest.java
@@ -1,8 +1,28 @@
-package test.weloveclouds.client.communication.services;
+package test.weloveclouds.communication.services;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import weloveclouds.communication.SocketFactory;
+import weloveclouds.communication.services.CommunicationService;
 
 /**
  * @author Benoit
  */
+@RunWith(MockitoJUnitRunner.class)
 public class CommunicationServiceTest {
+    private CommunicationService communicationService;
 
+    @Mock
+    SocketFactory socketFactory;
+
+    @Before
+    public void setUp(){
+        communicationService = new CommunicationService(socketFactory);
+    }
 }
