89cf992f78277e327f3f0097a887afdc6ae3bbad
Benoit Anctil-Robitaille <benoit.robitaille@hotmail.ca>
2016-10-23 18:46:54 +0200

    + Added base tests for communication service, replaced junit4 by junit4.5 included mockito

--- Echo Client/src/weloveclouds/communication/services/CommunicationService.java
+++ Echo Client/src/weloveclouds/communication/services/CommunicationService.java
@@ -1,6 +1,9 @@
 package weloveclouds.communication.services;
 
+import org.mockito.internal.matchers.Null;
+
 import static weloveclouds.communication.models.ConnectionState.CONNECTED;
+import static weloveclouds.communication.models.ConnectionState.DISCONNECTED;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -16,88 +19,91 @@ import weloveclouds.communication.models.ServerConnectionInfo;
  * @author Benoit, Benedek
  */
 public class CommunicationService {
-  private Connection connectionToServer;
-  private Thread connectionShutdownHook;
+    private Connection connectionToServer;
+    private Thread connectionShutdownHook;
 
-  private SocketFactory socketFactory;
+    private SocketFactory socketFactory;
 
-  public CommunicationService(SocketFactory socketFactory) {
-    this.connectionToServer = null;
-    this.socketFactory = socketFactory;
-  }
-
-  public void connectTo(ServerConnectionInfo remoteServer)
-      throws IOException, AlreadyConnectedException {
-    if (connectionToServer == null) {
-      initializeConnection(remoteServer);
-    } else {
-      switch (connectionToServer.getState()) {
-        case CONNECTED:
-          throw new AlreadyConnectedException();
-        case DISCONNECTED:
-          Runtime.getRuntime().removeShutdownHook(connectionShutdownHook);
-          initializeConnection(remoteServer);
-      }
+    public CommunicationService(SocketFactory socketFactory) {
+        this.connectionToServer = new Connection.ConnectionBuilder().build();
+        this.socketFactory = socketFactory;
     }
-  }
 
-  private void initializeConnection(ServerConnectionInfo remoteServer) throws IOException {
-    connectionToServer = new Connection.ConnectionBuilder().remoteServer(remoteServer)
-        .socket(socketFactory.createTcpSocketFromInfo(remoteServer)).build();
-
-    // create shutdown hook to automatically close the connection
-    connectionShutdownHook = new Thread(new ConnectionCloser(connectionToServer));
-    Runtime.getRuntime().addShutdownHook(connectionShutdownHook);
-  }
-
-  public void disconnect() throws IOException {
-    connectionToServer.kill();
-  }
-
-  public void send(byte[] content) throws IOException, UnableToSendRequestToServerException {
-    if (connectionToServer.getState() == CONNECTED) {
-      connectionToServer.getSocket().getOutputStream().write(content);
-    } else {
-      throw new ClientNotConnectedException();
+    public Connection getConnectionToServer() {
+        return connectionToServer;
     }
-  }
 
-  public byte[] receive() throws IOException, ClientNotConnectedException {
-    if (connectionToServer.getState() == CONNECTED) {
-      byte[] receivedData = null;
-
-      InputStream socketDataReader = connectionToServer.getSocket().getInputStream();
-
-      while (receivedData == null) {
-        if (socketDataReader.available() != 0) {
-          receivedData = new byte[socketDataReader.available()];
-          socketDataReader.read(receivedData);
+    public void connectTo(ServerConnectionInfo remoteServer)
+            throws IOException, AlreadyConnectedException {
+        if (connectionToServer.getState().equals(DISCONNECTED)) {
+            try {
+                Runtime.getRuntime().removeShutdownHook(connectionShutdownHook);
+            }catch(IllegalStateException | NullPointerException e){
+                //No hook previously added
+            }
+            initializeConnection(remoteServer);
+        } else {
+            throw new AlreadyConnectedException();
         }
-      }
-
-      return receivedData;
-    } else {
-      throw new ClientNotConnectedException();
-    }
-  }
-
-  private static class ConnectionCloser implements Runnable {
-    private Connection connection;
-
-    public ConnectionCloser(Connection connection) {
-      this.connection = connection;
     }
 
-    @Override
-    public void run() {
-      if (connection.getState() == CONNECTED) {
-        try {
-          connection.kill();
-        } catch (IOException e) {
-          // suppress exception because the thread is invoked as soon as JVM is to be shut down
+    private void initializeConnection(ServerConnectionInfo remoteServer) throws IOException {
+        connectionToServer = new Connection.ConnectionBuilder().remoteServer(remoteServer)
+                .socket(socketFactory.createTcpSocketFromInfo(remoteServer)).build();
+
+        // create shutdown hook to automatically close the connection
+        connectionShutdownHook = new Thread(new ConnectionCloser(connectionToServer));
+        Runtime.getRuntime().addShutdownHook(connectionShutdownHook);
+    }
+
+    public void disconnect() throws IOException {
+        connectionToServer.kill();
+    }
+
+    public void send(byte[] content) throws IOException, UnableToSendRequestToServerException {
+        if (connectionToServer.getState() == CONNECTED) {
+            connectionToServer.getSocket().getOutputStream().write(content);
+        } else {
+            throw new ClientNotConnectedException();
         }
-      }
     }
 
-  }
+    public byte[] receive() throws IOException, ClientNotConnectedException {
+        if (connectionToServer.getState() == CONNECTED) {
+            byte[] receivedData = null;
+
+            InputStream socketDataReader = connectionToServer.getSocket().getInputStream();
+
+            while (receivedData == null) {
+                if (socketDataReader.available() != 0) {
+                    receivedData = new byte[socketDataReader.available()];
+                    socketDataReader.read(receivedData);
+                }
+            }
+
+            return receivedData;
+        } else {
+            throw new ClientNotConnectedException();
+        }
+    }
+
+    private static class ConnectionCloser implements Runnable {
+        private Connection connection;
+
+        public ConnectionCloser(Connection connection) {
+            this.connection = connection;
+        }
+
+        @Override
+        public void run() {
+            if (connection.getState() == CONNECTED) {
+                try {
+                    connection.kill();
+                } catch (IOException e) {
+                    // suppress exception because the thread is invoked as soon as JVM is to be shut down
+                }
+            }
+        }
+
+    }
 }
--- Echo Client/test/test/weloveclouds/communication/services/CommunicationServiceTest.java
+++ Echo Client/test/test/weloveclouds/communication/services/CommunicationServiceTest.java
@@ -1,6 +1,9 @@
 package test.weloveclouds.communication.services;
 
 import static org.fest.assertions.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+import static org.mockito.Matchers.any;
+import static weloveclouds.communication.models.ConnectionState.DISCONNECTED;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -8,7 +11,16 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+
 import weloveclouds.communication.SocketFactory;
+import weloveclouds.communication.exceptions.AlreadyConnectedException;
+import weloveclouds.communication.exceptions.ClientNotConnectedException;
+import weloveclouds.communication.models.ServerConnectionInfo;
 import weloveclouds.communication.services.CommunicationService;
 
 /**
@@ -16,13 +28,109 @@ import weloveclouds.communication.services.CommunicationService;
  */
 @RunWith(MockitoJUnitRunner.class)
 public class CommunicationServiceTest {
+    private static final String VALID_SERVER_IP_ADDRESS = "131.159.52.1";
+    private static final String INVALID_SERVER_IP_ADDRESS = "127.0.0.1";
+    private static final int VALID_SERVER_PORT = 50000;
+    private static final boolean CONNECTED = true;
     private CommunicationService communicationService;
+    private ServerConnectionInfo validServerConnectionInfos;
+    private ServerConnectionInfo invalidServerConnectionInfos;
+    private Socket socketFromValidServerInfos;
+    private Socket socketFromInvalidServerInfos;
 
     @Mock
-    SocketFactory socketFactory;
+    Socket socketMock;
+    @Mock
+    SocketFactory socketFactoryMock;
 
     @Before
-    public void setUp(){
-        communicationService = new CommunicationService(socketFactory);
+    public void setUp() throws Exception {
+        communicationService = new CommunicationService(socketFactoryMock);
+
+        validServerConnectionInfos = new ServerConnectionInfo(InetAddress.getByName
+                (VALID_SERVER_IP_ADDRESS), VALID_SERVER_PORT);
+        invalidServerConnectionInfos = new ServerConnectionInfo(InetAddress.getByName
+                (INVALID_SERVER_IP_ADDRESS), VALID_SERVER_PORT);
+
+        /*
+        socketFromValidServerInfos = new Socket(validServerConnectionInfos.getIpAddress(),
+                validServerConnectionInfos.getPort());
+        socketFromInvalidServerInfos = new Socket(invalidServerConnectionInfos.getIpAddress(),
+                invalidServerConnectionInfos.getPort());*/
+
+        when(socketFactoryMock.createTcpSocketFromInfo(validServerConnectionInfos))
+                .thenReturn(socketFromValidServerInfos);
+
+        when(socketFactoryMock.createTcpSocketFromInfo(invalidServerConnectionInfos))
+                .thenReturn(socketFromInvalidServerInfos);
+    }
+
+    @Test
+    public void shouldConnectToRemoteServerUsingValidServerInfos() throws Exception {
+        assertThat(communicationService.getConnectionToServer().getState()).isEqualTo(DISCONNECTED);
+
+        communicationService.connectTo(validServerConnectionInfos);
+
+        assertThat(communicationService.getConnectionToServer().getState()).isEqualTo(CONNECTED);
+    }
+
+    @Test(expected = IOException.class)
+    public void shouldThrowWhenConnectingToRemoteServerUsingInvalidServerInfos() throws Exception {
+        assertThat(communicationService.getConnectionToServer().getState()).isEqualTo(DISCONNECTED);
+
+        try {
+            communicationService.connectTo(invalidServerConnectionInfos);
+        } finally {
+            assertThat(communicationService.getConnectionToServer().getState()).isEqualTo(DISCONNECTED);
+        }
+    }
+
+    @Test
+    public void shouldCloseTheConnectionOnDisconnect() throws Exception {
+        assertThat(communicationService.getConnectionToServer().getState()).isEqualTo(DISCONNECTED);
+
+        communicationService.connectTo(validServerConnectionInfos);
+
+        assertThat(communicationService.getConnectionToServer().getState()).isEqualTo(CONNECTED);
+
+        communicationService.disconnect();
+
+        assertThat(communicationService.getConnectionToServer().getState()).isEqualTo(DISCONNECTED);
+    }
+
+    @Test(expected = AlreadyConnectedException.class)
+    public void shouldThrowWhenTryingToConnectToServerButIsAlreadyConnected() throws Exception {
+        assertThat(communicationService.getConnectionToServer().getState()).isEqualTo(DISCONNECTED);
+
+        communicationService.connectTo(validServerConnectionInfos);
+
+        assertThat(communicationService.getConnectionToServer().getState()).isEqualTo(CONNECTED);
+
+        communicationService.connectTo(validServerConnectionInfos);
+    }
+
+    @Test(expected = ClientNotConnectedException.class)
+    public void shouldThrowWhenTryingToSendWithoutBeingConnectedToServer() throws Exception {
+        String message = "Should not be send";
+        communicationService.send(message.getBytes());
+    }
+
+    @Test(expected = ClientNotConnectedException.class)
+    public void shouldThrowWhenTryingToReceiveWithoutBeingConnectedToServer() throws Exception {
+        communicationService.receive();
+    }
+
+    @Test
+    public void shouldReadTheReceivedDataOnReception() throws Exception {
+        final String RECEIVED_MESSAGE = "RECEIVED DATA";
+        InputStream mockedSocketInputStream = new ByteArrayInputStream(RECEIVED_MESSAGE.getBytes());
+        when(socketFactoryMock.createTcpSocketFromInfo(any(ServerConnectionInfo.class)))
+                .thenReturn(socketMock);
+        when(socketMock.getInputStream()).thenReturn(mockedSocketInputStream);
+        when(socketMock.isConnected()).thenReturn(CONNECTED);
+
+        communicationService.connectTo(validServerConnectionInfos);
+
+        assertThat(new String(communicationService.receive())).isEqualTo(RECEIVED_MESSAGE);
     }
 }
