3078774789a8640a72853820e27b5f9c6bcd740d
Benoit Anctil-Robitaille <benoit.robitaille@hotmail.ca>
2016-10-25 18:26:23 +0200

    + Added command validation

---
+++ Echo Client/src/weloveclouds/client/utils/ArgumentsValidator.java
@@ -0,0 +1,67 @@
+package weloveclouds.client.utils;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.security.InvalidParameterException;
+import java.util.Arrays;
+import java.util.List;
+
+import weloveclouds.communication.models.ServerConnectionInfo;
+
+/**
+ * @author Benedek
+ */
+public class UserInputValidator {
+    private static final String EMPTY_MESSAGE_ERROR_MESSAGE = "Message cannot be empty (null).";
+    private static final int CONNECT_NUMBER_OF_ARGUMENTS = 2;
+    private static final int LEVEL_INDEX = 0;
+    private static List<String> logLevels =
+            Arrays.asList("ALL", "DEBUG", "INFO", "WARN", "ERROR", "FATAL", "OFF");
+
+    public static void validateConnectArguments(String[] arguments, ServerConnectionInfo
+            remoteServer) {
+        if (ValidatorUtils.isNullOrEmpty(arguments) || arguments.length !=
+                CONNECT_NUMBER_OF_ARGUMENTS) {
+            throw new InvalidParameterException("Command need arguments (<IP address> and <port>)" +
+                    " only");
+        } else if (remoteServer.getPort() < 0 || remoteServer.getPort() > 65536) {
+            throw new IllegalArgumentException("Port should be in the range [0,65536].");
+        }
+    }
+
+    public static void validateSendArgument(String[] arguments) throws IllegalArgumentException {
+        if (ValidatorUtils.isNullOrEmpty(arguments)) {
+            throw new IllegalArgumentException(EMPTY_MESSAGE_ERROR_MESSAGE);
+        }
+    }
+
+    public static void validateLogLevelArgument(String[] arguments) throws IllegalArgumentException {
+        if (!logLevels.contains(arguments[LEVEL_INDEX])) {
+            throw new IllegalArgumentException(
+                    "Log level is not recognized. It should be capitalized and should be one of the followings: All, DEBUG, INFO, WARN, ERROR, FATAL, OFF");
+        }
+    }
+
+    public static void validateDisconnectArgument(String[] arguments) throws
+            IllegalArgumentException {
+        if (!isNullOrEmpty(arguments)) {
+            throw new IllegalArgumentException("Command does not accept any argument.");
+        }
+    }
+
+    public static void validateHelpArgument(String[] arguments) {
+        if (!isNullOrEmpty(arguments)) {
+            throw new IllegalArgumentException("Command does not accept any argument.");
+        }
+    }
+
+    public static void validateQuitArgument(String[] arguments) {
+        if (!isNullOrEmpty(arguments)) {
+            throw new IllegalArgumentException("Command does not accept any argument.");
+        }
+    }
+
+    public static boolean isNullOrEmpty(String[] arguments) {
+        return arguments == null || arguments.length == 0;
+    }
+}
---
+++ Echo Client/src/weloveclouds/client/utils/ValidatorUtils.java
@@ -0,0 +1,10 @@
+package weloveclouds.client.utils;
+
+/**
+ * Created by Benoit on 2016-10-25.
+ */
+public class CommandValidatorUtils {
+    public static boolean isNullOrEmpty(String text) {
+        return text == null || text.isEmpty();
+    }
+}
