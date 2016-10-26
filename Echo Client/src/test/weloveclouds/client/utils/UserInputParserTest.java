34d819826643f0167f283c87d984ef5807235284
Benoit Anctil-Robitaille <benoit.robitaille@hotmail.ca>
2016-10-26 12:00:31 +0200

    + Finished communication module test and started userinputparsertest

--- Echo Client/src/test/weloveclouds/client/utils/UserInputParserTest.java
+++ Echo Client/src/test/weloveclouds/client/utils/UserInputParserTest.java
@@ -1,11 +1,10 @@
 package test.weloveclouds.client.utils;
 
-import static org.fest.assertions.Assertions.assertThat;
+import static org.fest.assertions.Assertions.*;
 
 import org.junit.Test;
 
-import com.google.common.base.Joiner;
-
+import weloveclouds.client.models.Command;
 import weloveclouds.client.models.ParsedUserInput;
 import weloveclouds.client.utils.UserInputParser;
 
@@ -13,19 +12,49 @@ import weloveclouds.client.utils.UserInputParser;
  * @author Benoit
  */
 public class UserInputParserTest {
-  private static final String SEND_COMMAND_CAPITALIZE = "SEND";
-  private static final String SEND_COMMAND = "send";
-  private static final String PAYLOAD = "An awesome Payload";
-  private static final String API_VALID_USER_INPUT_NON_CAPITAL_LETTER =
-      Joiner.on(" ").join(SEND_COMMAND, PAYLOAD);
-  private static final String API_VALID_USER_INPUT_CAPITAL_LETTER =
-      Joiner.on(" ").join(SEND_COMMAND_CAPITALIZE, PAYLOAD);
+    private static final String SEND_COMMAND = "send";
+    private static final String CONNECT_COMMAND = "connect";
+    private static final String DISCONNECT_COMMAND = "disconnect";
+    private static final String HELP_COMMAND = "help";
+    private static final String QUIT_COMMAND = "quit";
+    private static final String LOGLEVEL_COMMAND = "loglevel";
+    private static final String INVALID_COMMAND = "lukeskywalker";
+    private static final String SINGLE_ARGUMENTS = "testargs123:";
+    private static final String MULTIPLE_ARGUMENTS = "args1 args2 args3";
 
-  @Test
-  public void shouldCreateARequestOnValidUserInput() throws Exception {
-    ParsedUserInput userRequest = UserInputParser.parse(API_VALID_USER_INPUT_NON_CAPITAL_LETTER);
-    assertThat(userRequest.getCommand()).isEqualTo(SEND_COMMAND);
-    assertThat(userRequest.getArguments()).isEqualTo(null);
-  }
+    @Test
+    public void shouldParseValidCommandsWithoutArguments() {
+        verifyValidCommandParsingWithoutArguments(SEND_COMMAND, Command.SEND);
+        verifyValidCommandParsingWithoutArguments(CONNECT_COMMAND, Command.CONNECT);
+        verifyValidCommandParsingWithoutArguments(DISCONNECT_COMMAND, Command.DISCONNECT);
+        verifyValidCommandParsingWithoutArguments(HELP_COMMAND, Command.HELP);
+        verifyValidCommandParsingWithoutArguments(QUIT_COMMAND, Command.QUIT);
+        verifyValidCommandParsingWithoutArguments(LOGLEVEL_COMMAND, Command.LOGLEVEL);
+        verifyValidCommandParsingWithoutArguments(INVALID_COMMAND, Command.DEFAULT);
+    }
 
+    @Test void shouldParseValidCommandsWithSingleArguments(){
+        verifyValidCommandParsingWithoutArguments(SEND_COMMAND, Command.SEND);
+        verifyValidCommandParsingWithoutArguments(CONNECT_COMMAND, Command.CONNECT);
+        verifyValidCommandParsingWithoutArguments(DISCONNECT_COMMAND, Command.DISCONNECT);
+        verifyValidCommandParsingWithoutArguments(HELP_COMMAND, Command.HELP);
+        verifyValidCommandParsingWithoutArguments(QUIT_COMMAND, Command.QUIT);
+        verifyValidCommandParsingWithoutArguments(LOGLEVEL_COMMAND, Command.LOGLEVEL);
+        verifyValidCommandParsingWithoutArguments(INVALID_COMMAND, Command.DEFAULT);
+    }
+
+    private void verifyValidCommandParsingWithoutArguments(String userInput, Command expectedCommand){
+        ParsedUserInput parsedUserInput = UserInputParser.parse(userInput);
+        assertThat(parsedUserInput.getCommand()).isEqualTo(expectedCommand);
+        assertThat(parsedUserInput.getArguments()).isNullOrEmpty();
+    }
+
+    private void verifyValidCommandParsingWithSingleArguments(String userInput, Command
+            expectedCommand, String argument){
+        final int SINGLE_ARGUMENT = 1;
+        ParsedUserInput parsedUserInput = UserInputParser.parse(userInput);
+        assertThat(parsedUserInput.getCommand()).isEqualTo(expectedCommand);
+        assertThat(parsedUserInput.getArguments().length).isEqualTo(SINGLE_ARGUMENT);
+        assertThat(parsedUserInput.getArguments()).contains(argument);
+    }
 }
