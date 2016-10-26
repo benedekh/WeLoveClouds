e964b4bf0bc8603379cf7eaf33a486c8b688f207
benedekh <benedekh12@gmail.com>
2016-10-26 12:52:40 +0200

    removing warning from test code

--- Echo Client/src/test/weloveclouds/client/utils/UserInputParserTest.java
+++ Echo Client/src/test/weloveclouds/client/utils/UserInputParserTest.java
@@ -13,19 +13,17 @@ import weloveclouds.client.utils.UserInputParser;
  * @author Benoit
  */
 public class UserInputParserTest {
-  private static final String SEND_COMMAND_CAPITALIZE = "SEND";
-  private static final String SEND_COMMAND = "send";
-  private static final String PAYLOAD = "An awesome Payload";
-  private static final String API_VALID_USER_INPUT_NON_CAPITAL_LETTER =
-      Joiner.on(" ").join(SEND_COMMAND, PAYLOAD);
-  private static final String API_VALID_USER_INPUT_CAPITAL_LETTER =
-      Joiner.on(" ").join(SEND_COMMAND_CAPITALIZE, PAYLOAD);
+    private static final String SEND_COMMAND = "send";
+    private static final String PAYLOAD = "An awesome Payload";
+    private static final String API_VALID_USER_INPUT_NON_CAPITAL_LETTER =
+            Joiner.on(" ").join(SEND_COMMAND, PAYLOAD);
 
-  @Test
-  public void shouldCreateARequestOnValidUserInput() throws Exception {
-    ParsedUserInput userRequest = UserInputParser.parse(API_VALID_USER_INPUT_NON_CAPITAL_LETTER);
-    assertThat(userRequest.getCommand()).isEqualTo(SEND_COMMAND);
-    assertThat(userRequest.getArguments()).isEqualTo(null);
-  }
+    @Test
+    public void shouldCreateARequestOnValidUserInput() throws Exception {
+        ParsedUserInput userRequest =
+                UserInputParser.parse(API_VALID_USER_INPUT_NON_CAPITAL_LETTER);
+        assertThat(userRequest.getCommand()).isEqualTo(SEND_COMMAND);
+        assertThat(userRequest.getArguments()).isEqualTo(null);
+    }
 
 }
--- Echo Client/src/test/weloveclouds/communication/services/CommunicationServiceTest.java
+++ Echo Client/src/test/weloveclouds/communication/services/CommunicationServiceTest.java
@@ -21,7 +21,6 @@ import org.mockito.runners.MockitoJUnitRunner;
 import weloveclouds.communication.SocketFactory;
 import weloveclouds.communication.exceptions.AlreadyConnectedException;
 import weloveclouds.communication.exceptions.ClientNotConnectedException;
-import weloveclouds.communication.models.Connection;
 import weloveclouds.communication.models.ServerConnectionInfo;
 import weloveclouds.communication.services.CommunicationService;
 
@@ -30,110 +29,111 @@ import weloveclouds.communication.services.CommunicationService;
  */
 @RunWith(MockitoJUnitRunner.class)
 public class CommunicationServiceTest {
-  private static final String VALID_SERVER_IP_ADDRESS = "131.159.52.2";
-  private static final String INVALID_SERVER_IP_ADDRESS = "127.0.0.1";
-  private static final int VALID_SERVER_PORT = 50000;
-  private static final boolean CONNECTED_FLAG = true;
-  private CommunicationService communicationService;
-  private ServerConnectionInfo validServerConnectionInfos;
-  private ServerConnectionInfo invalidServerConnectionInfos;
-  private Socket socketFromValidServerInfos;
-  private Socket socketFromInvalidServerInfos;
+    private static final String VALID_SERVER_IP_ADDRESS = "131.159.52.2";
+    private static final String INVALID_SERVER_IP_ADDRESS = "127.0.0.1";
+    private static final int VALID_SERVER_PORT = 50000;
+    private static final boolean CONNECTED_FLAG = true;
+    private CommunicationService communicationService;
+    private ServerConnectionInfo validServerConnectionInfos;
+    private ServerConnectionInfo invalidServerConnectionInfos;
+    private Socket socketFromValidServerInfos;
+    private Socket socketFromInvalidServerInfos;
 
-  @Mock
-  Socket socketMock;
-  @Mock
-  SocketFactory socketFactoryMock;
+    @Mock
+    Socket socketMock;
+    @Mock
+    SocketFactory socketFactoryMock;
 
-  @Before
-  public void setUp() throws Exception {
-    communicationService = new CommunicationService(socketFactoryMock);
+    @Before
+    public void setUp() throws Exception {
+        communicationService = new CommunicationService(socketFactoryMock);
 
-    validServerConnectionInfos = new ServerConnectionInfo.ServerConnectionInfoBuilder()
-        .ipAddress(InetAddress.getByName(VALID_SERVER_IP_ADDRESS)).port(VALID_SERVER_PORT).build();
-    invalidServerConnectionInfos = new ServerConnectionInfo.ServerConnectionInfoBuilder()
-        .ipAddress(InetAddress.getByName(INVALID_SERVER_IP_ADDRESS)).port(VALID_SERVER_PORT)
-        .build();
+        validServerConnectionInfos = new ServerConnectionInfo.ServerConnectionInfoBuilder()
+                .ipAddress(InetAddress.getByName(VALID_SERVER_IP_ADDRESS)).port(VALID_SERVER_PORT)
+                .build();
+        invalidServerConnectionInfos = new ServerConnectionInfo.ServerConnectionInfoBuilder()
+                .ipAddress(InetAddress.getByName(INVALID_SERVER_IP_ADDRESS)).port(VALID_SERVER_PORT)
+                .build();
 
-    socketFromValidServerInfos =
-        new Socket(validServerConnectionInfos.getIpAddress(), validServerConnectionInfos.getPort());
+        socketFromValidServerInfos = new Socket(validServerConnectionInfos.getIpAddress(),
+                validServerConnectionInfos.getPort());
 
-    when(socketFactoryMock.createTcpSocketFromInfo(validServerConnectionInfos))
-        .thenReturn(socketFromValidServerInfos);
+        when(socketFactoryMock.createTcpSocketFromInfo(validServerConnectionInfos))
+                .thenReturn(socketFromValidServerInfos);
 
-    when(socketFactoryMock.createTcpSocketFromInfo(invalidServerConnectionInfos))
-        .thenReturn(socketFromInvalidServerInfos);
-  }
-
-  @Test
-  public void shouldConnectToRemoteServerUsingValidServerInfos() throws Exception {
-    assertFalse(communicationService.isConnected());
-
-    communicationService.connectTo(validServerConnectionInfos);
-
-    assertTrue(communicationService.isConnected());
-  }
-
-  @Test(expected = IOException.class)
-  public void shouldThrowWhenConnectingToRemoteServerUsingInvalidServerInfos() throws Exception {
-    socketFromInvalidServerInfos = new Socket(invalidServerConnectionInfos.getIpAddress(),
-        invalidServerConnectionInfos.getPort());
-
-    assertFalse(communicationService.isConnected());
-
-    try {
-      communicationService.connectTo(invalidServerConnectionInfos);
-    } finally {
-      assertFalse(communicationService.isConnected());
+        when(socketFactoryMock.createTcpSocketFromInfo(invalidServerConnectionInfos))
+                .thenReturn(socketFromInvalidServerInfos);
     }
-  }
 
-  @Test
-  public void shouldCloseTheConnectionOnDisconnect() throws Exception {
-    assertFalse(communicationService.isConnected());
+    @Test
+    public void shouldConnectToRemoteServerUsingValidServerInfos() throws Exception {
+        assertFalse(communicationService.isConnected());
 
-    communicationService.connectTo(validServerConnectionInfos);
+        communicationService.connectTo(validServerConnectionInfos);
 
-    assertTrue(communicationService.isConnected());
+        assertTrue(communicationService.isConnected());
+    }
 
-    communicationService.disconnect();
+    @Test(expected = IOException.class)
+    public void shouldThrowWhenConnectingToRemoteServerUsingInvalidServerInfos() throws Exception {
+        socketFromInvalidServerInfos = new Socket(invalidServerConnectionInfos.getIpAddress(),
+                invalidServerConnectionInfos.getPort());
 
-    assertFalse(communicationService.isConnected());
-  }
+        assertFalse(communicationService.isConnected());
 
-  @Test(expected = AlreadyConnectedException.class)
-  public void shouldThrowWhenTryingToConnectToServerButIsAlreadyConnected() throws Exception {
-    assertFalse(communicationService.isConnected());
+        try {
+            communicationService.connectTo(invalidServerConnectionInfos);
+        } finally {
+            assertFalse(communicationService.isConnected());
+        }
+    }
 
-    communicationService.connectTo(validServerConnectionInfos);
+    @Test
+    public void shouldCloseTheConnectionOnDisconnect() throws Exception {
+        assertFalse(communicationService.isConnected());
 
-    assertTrue(communicationService.isConnected());
+        communicationService.connectTo(validServerConnectionInfos);
 
-    communicationService.connectTo(validServerConnectionInfos);
-  }
+        assertTrue(communicationService.isConnected());
 
-  @Test(expected = ClientNotConnectedException.class)
-  public void shouldThrowWhenTryingToSendWithoutBeingConnectedToServer() throws Exception {
-    String message = "Should not be send";
-    communicationService.send(message.getBytes());
-  }
+        communicationService.disconnect();
 
-  @Test(expected = ClientNotConnectedException.class)
-  public void shouldThrowWhenTryingToReceiveWithoutBeingConnectedToServer() throws Exception {
-    communicationService.receive();
-  }
+        assertFalse(communicationService.isConnected());
+    }
 
-  @Test
-  public void shouldReadTheReceivedDataOnReception() throws Exception {
-    final String RECEIVED_MESSAGE = "RECEIVED DATA";
-    InputStream mockedSocketInputStream = new ByteArrayInputStream(RECEIVED_MESSAGE.getBytes());
-    when(socketFactoryMock.createTcpSocketFromInfo(any(ServerConnectionInfo.class)))
-        .thenReturn(socketMock);
-    when(socketMock.getInputStream()).thenReturn(mockedSocketInputStream);
-    when(socketMock.isConnected()).thenReturn(CONNECTED_FLAG);
+    @Test(expected = AlreadyConnectedException.class)
+    public void shouldThrowWhenTryingToConnectToServerButIsAlreadyConnected() throws Exception {
+        assertFalse(communicationService.isConnected());
 
-    communicationService.connectTo(validServerConnectionInfos);
+        communicationService.connectTo(validServerConnectionInfos);
 
-    assertThat(new String(communicationService.receive())).isEqualTo(RECEIVED_MESSAGE);
-  }
+        assertTrue(communicationService.isConnected());
+
+        communicationService.connectTo(validServerConnectionInfos);
+    }
+
+    @Test(expected = ClientNotConnectedException.class)
+    public void shouldThrowWhenTryingToSendWithoutBeingConnectedToServer() throws Exception {
+        String message = "Should not be send";
+        communicationService.send(message.getBytes());
+    }
+
+    @Test(expected = ClientNotConnectedException.class)
+    public void shouldThrowWhenTryingToReceiveWithoutBeingConnectedToServer() throws Exception {
+        communicationService.receive();
+    }
+
+    @Test
+    public void shouldReadTheReceivedDataOnReception() throws Exception {
+        final String RECEIVED_MESSAGE = "RECEIVED DATA";
+        InputStream mockedSocketInputStream = new ByteArrayInputStream(RECEIVED_MESSAGE.getBytes());
+        when(socketFactoryMock.createTcpSocketFromInfo(any(ServerConnectionInfo.class)))
+                .thenReturn(socketMock);
+        when(socketMock.getInputStream()).thenReturn(mockedSocketInputStream);
+        when(socketMock.isConnected()).thenReturn(CONNECTED_FLAG);
+
+        communicationService.connectTo(validServerConnectionInfos);
+
+        assertThat(new String(communicationService.receive())).isEqualTo(RECEIVED_MESSAGE);
+    }
 }
