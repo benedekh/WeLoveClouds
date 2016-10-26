21c20c1f4538010d3d6c54ed19381d5c9ea13984
benedekh <benedekh12@gmail.com>
2016-10-26 17:46:59 +0200

    Command model is restored to previous version.

--- Echo Client/src/weloveclouds/client/models/Command.java
+++ Echo Client/src/weloveclouds/client/models/Command.java
@@ -1,5 +1,8 @@
 package weloveclouds.client.models;
 
+import java.util.Set;
+import java.util.TreeSet;
+
 import org.apache.log4j.Logger;
 
 import weloveclouds.client.utils.StringJoiner;
@@ -15,19 +18,30 @@ public enum Command {
 
     private static final Logger LOGGER = Logger.getLogger(Command.class);
 
+    private static final Set<String> lowercaseCommands = getCommandsAsLowercase();
+
+    /**
+     * Creates a set from the lower-case names of the commands.
+     */
+    private static Set<String> getCommandsAsLowercase() {
+        Set<String> names = new TreeSet<>();
+        for (Command command : Command.values()) {
+            names.add(command.name().toLowerCase());
+        }
+        return names;
+    }
+
     /**
      * Converts the parameter to a command if its name matches with one of the commands. Otherwise
      * it returns {@link #DEFAULT}
      */
     public static Command fromString(String command) {
-        Command enumValue;
-        try {
-            // TODO refactor it for exact match not toUppercase!!!!
-            enumValue = Command.valueOf(command.toUpperCase());
-        } catch (IllegalArgumentException ex) {
-            enumValue = DEFAULT;
+        Command recognized = (command == null || !lowercaseCommands.contains(command) ? DEFAULT
+                : Command.valueOf(command));
+        if (recognized == DEFAULT) {
             LOGGER.warn(StringJoiner.join("", "Command (", command, ") is not recognized."));
         }
-        return enumValue;
+        return recognized;
     }
+
 }
