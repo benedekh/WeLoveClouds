d9c72dcf150ee065e7e61363def351773edff943
Benoit Anctil-Robitaille <benoit.robitaille@hotmail.ca>
2016-10-29 18:56:54 +0200

    + Added simple connection handler and server class

---
+++ Basic_Storage_Server/src/weloveclouds/server/core/SimpleConnectionHandler.java
@@ -0,0 +1,7 @@
+package weloveclouds.server.core;
+
+/**
+ * Created by Benoit on 2016-10-29.
+ */
+public class ConnectionHandler {
+}
---
+++ Basic_Storage_Server/src/weloveclouds/server/models/Connection.java
@@ -0,0 +1,7 @@
+package weloveclouds.server.models;
+
+/**
+ * Created by Benoit on 2016-10-29.
+ */
+public class Connection {
+}
