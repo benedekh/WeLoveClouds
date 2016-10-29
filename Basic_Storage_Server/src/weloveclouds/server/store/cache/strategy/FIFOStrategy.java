02eb201fc54d4a70ad32d247549e61efd4045ad0
benedekh <benedekh12@gmail.com>
2016-10-29 20:47:44 +0200

    FIFO Strategy impl

--- Basic_Storage_Server/src/weloveclouds/server/store/cache/strategy/FIFOStrategy.java
+++ Basic_Storage_Server/src/weloveclouds/server/store/cache/strategy/FIFOStrategy.java
@@ -1,32 +1,51 @@
 package weloveclouds.server.store.cache.strategy;
 
+import java.util.ArrayDeque;
+import java.util.NoSuchElementException;
+import java.util.Queue;
+
 import weloveclouds.kvstore.KVEntry;
 import weloveclouds.server.store.exceptions.StorageException;
 
 public class FIFOStrategy implements DisplacementStrategy {
 
+    private Queue<KVEntry> fifo;
+
+    public FIFOStrategy() {
+        this.fifo = new ArrayDeque<>();
+    }
+
     @Override
     public KVEntry displaceEntry() throws StorageException {
-        // TODO Auto-generated method stub
-        return null;
+        try {
+            return fifo.remove();
+        } catch (NoSuchElementException ex) {
+            throw new StorageException("FIFO is empty so it cannot remove anything.");
+        }
     }
 
     @Override
     public void putEntry(KVEntry entry) throws StorageException {
-        // TODO Auto-generated method stub
-
+        try {
+            fifo.add(entry);
+        } catch (NullPointerException ex) {
+            throw new StorageException("Entry cannot be null.");
+        }
     }
 
     @Override
     public String getValue(String key) throws StorageException {
-        // TODO Auto-generated method stub
-        return null;
+        // FIFO strategy does not update anything
+        return "";
     }
 
     @Override
     public void removeEntry(String key) throws StorageException {
-        // TODO Auto-generated method stub
-
+        try {
+            fifo.remove(key);
+        } catch (NullPointerException ex) {
+            throw new StorageException("Key cannot be null.");
+        }
     }
 
 }
