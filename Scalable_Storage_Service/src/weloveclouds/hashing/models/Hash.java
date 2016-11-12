73e456ea8b7d98ed8fd50125111381ba2fd4ba49
benedekh <benedekh12@gmail.com>
2016-11-12 15:49:52 +0100

    HashRange class introduced, Hash.compareTo fixed

--- Scalable_Storage_Service/src/weloveclouds/hashing/models/Hash.java
+++ Scalable_Storage_Service/src/weloveclouds/hashing/models/Hash.java
@@ -4,14 +4,28 @@ import weloveclouds.client.utils.CustomStringJoiner;
 
 public class Hash implements Comparable<Hash> {
 
+    public static final Hash MAX_VALUE;
+    public static final Hash MIN_VALUE;
+
+    static {
+        int numberOfBytes = 16;
+        byte[] maxValues = new byte[numberOfBytes];
+        byte[] minValues = new byte[numberOfBytes];
+
+        for (int i = 0; i < numberOfBytes; ++i) {
+            maxValues[i] = Byte.MAX_VALUE;
+            minValues[i] = Byte.MIN_VALUE;
+        }
+
+        MAX_VALUE = new Hash(maxValues);
+        MIN_VALUE = new Hash(minValues);
+    }
+
+
     private byte[] hash;
 
     public Hash(byte[] hash) {
         this.hash = hash;
-    }
-
-    public byte[] hash() {
-        return hash;
     }
 
     @Override
@@ -27,19 +41,27 @@ public class Hash implements Comparable<Hash> {
         }
 
         for (int i = 0; i < hash.length; ++i) {
-            int thisOctet = Math.abs(hash[i]); // to disregard the sign bit
-            int otherOctet = Math.abs(other.hash[i]); // to disregard the sign bit
+            int compareResult = Byte.compare(hash[i], other.hash[i]);
 
-            if (thisOctet == otherOctet) {
+            if (compareResult == 0) {
                 continue;
-            } else if (thisOctet < otherOctet) {
+            } else if (compareResult < 0) {
                 return LESS;
-            } else if (thisOctet > otherOctet) {
+            } else if (compareResult > 0) {
                 return GREATER;
             }
         }
 
         return EQUALS;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (byte b : hash) {
+            sb.append(CustomStringJoiner.join("", String.valueOf(b), "|"));
+        }
+        return sb.toString();
     }
 
 }
