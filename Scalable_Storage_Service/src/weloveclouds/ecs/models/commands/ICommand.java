015b4de9b2c086666013c95903c4032a4e08e755
HB <s3529366@student.rmit.edu.au>
2016-11-19 16:25:05 +0100

    Made ecs ICommand throw a ClientSideException instead of just an Exception

--- Scalable_Storage_Service/src/weloveclouds/ecs/models/commands/ICommand.java
+++ Scalable_Storage_Service/src/weloveclouds/ecs/models/commands/ICommand.java
@@ -1,11 +1,12 @@
 package weloveclouds.ecs.models.commands;
 
 import weloveclouds.ecs.models.commands.IValidatable;
+import weloveclouds.ecs.exceptions.ClientSideException;
 
 /**
  * Represents an object (command) that can be executed.
  *
- * @author Benoit, adapted to by hb
+ * @author Benoit, copied here by hb
  */
 public interface ICommand extends IValidatable {
 
@@ -14,6 +15,5 @@ public interface ICommand extends IValidatable {
      *
      * @throws ClientSideException if any error occurs
      */
-    void execute() throws Exception;
-    //TODO: write custom exception
+    void execute() throws ClientSideException;
 }
