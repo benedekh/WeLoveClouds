0ae50202f75b2db8a0014e79fceb5c9107f4f84e
Benoit Anctil-Robitaille <benoit.robitaille@hotmail.ca>
2016-11-19 14:00:25 +0100

    - Removed deleted class import

--- Scalable_Storage_Service/src/weloveclouds/ecs/services/ISecureShellService.java
+++ Scalable_Storage_Service/src/weloveclouds/ecs/services/ISecureShellService.java
@@ -1,11 +1,7 @@
 package weloveclouds.ecs.services;
 
-import com.jcraft.jsch.JSchException;
-
 import weloveclouds.ecs.exceptions.ssh.SecureShellServiceException;
 import weloveclouds.ecs.models.commands.ssh.AbstractRemoteCommand;
-import weloveclouds.ecs.models.ssh.AuthInfos;
-import weloveclouds.ecs.models.ssh.RemoteCommandExecutionRequest;
 
 /**
  * Created by Benoit on 2016-11-18.
