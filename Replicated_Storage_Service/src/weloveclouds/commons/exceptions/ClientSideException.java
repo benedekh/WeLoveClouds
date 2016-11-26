441485f21dc3861918ddaf9864def004571a6094
Benoit Anctil-Robitaille <benoit.robitaille@hotmail.ca>
2016-11-26 21:11:50 +0100

    + serialVersionUID to exception

--- Replicated_Storage_Service/src/weloveclouds/commons/exceptions/ClientSideException.java
+++ Replicated_Storage_Service/src/weloveclouds/commons/exceptions/ClientSideException.java
@@ -4,6 +4,8 @@ package weloveclouds.commons.exceptions;
  * Created by Benoit on 2016-11-17.
  */
 public class ClientSideException extends Exception {
+    private static final long serialVersionUID = 7333053297377130794L;
+
     public ClientSideException(String message) {
         super(message);
     }
--- Replicated_Storage_Service/src/weloveclouds/commons/exceptions/ServerSideException.java
+++ Replicated_Storage_Service/src/weloveclouds/commons/exceptions/ServerSideException.java
@@ -4,6 +4,8 @@ package weloveclouds.commons.exceptions;
  * Created by Benoit on 2016-11-19.
  */
 public class ServerSideException extends Exception {
+    private static final long serialVersionUID = 7764943152427779038L;
+
     public ServerSideException(String message) {
         super(message);
     }
