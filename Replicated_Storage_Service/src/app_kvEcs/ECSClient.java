d7267c0872c449c8b64b916d8170b7202caeacf2
Benoit Anctil-Robitaille <benoit.robitaille@hotmail.ca>
2016-12-10 12:48:18 +0100

    - Removed unused import

--- Replicated_Storage_Service/src/app_kvEcs/ECSClient.java
+++ Replicated_Storage_Service/src/app_kvEcs/ECSClient.java
@@ -3,37 +3,15 @@ package app_kvEcs;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 
-import com.jcraft.jsch.HASH;
-
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
 import java.io.IOException;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 import weloveclouds.commons.cli.utils.UserOutputWriter;
 import weloveclouds.commons.context.ExecutionContext;
-import weloveclouds.commons.hashing.models.Hash;
-import weloveclouds.commons.hashing.models.HashRange;
-import weloveclouds.commons.kvstore.deserialization.helper.HashRangeDeserializer;
-import weloveclouds.commons.kvstore.deserialization.helper.ServerConnectionInfoDeserializer;
-import weloveclouds.commons.kvstore.serialization.helper.HashRangeSerializer;
-import weloveclouds.commons.kvstore.serialization.helper.HashSerializer;
-import weloveclouds.commons.kvstore.serialization.helper.ServerConnectionInfoSerializer;
-import weloveclouds.commons.serialization.NodeHealthInfosSerializer;
-import weloveclouds.commons.serialization.RingTopologySerializer;
-import weloveclouds.commons.serialization.StorageNodeSerializer;
-import weloveclouds.commons.serialization.deserialization.NodeHealtInfosDeserializer;
-import weloveclouds.commons.serialization.deserialization.RingTopologyDeserializer;
-import weloveclouds.commons.serialization.deserialization.StorageNodeDeserializer;
-import weloveclouds.communication.models.ServerConnectionInfo;
 import weloveclouds.ecs.client.Client;
 import weloveclouds.ecs.contexts.EcsExecutionContext;
-import weloveclouds.ecs.models.repository.StorageNode;
-import weloveclouds.ecs.models.topology.RingTopology;
 import weloveclouds.ecs.modules.client.EcsClientModule;
 import weloveclouds.server.utils.LogSetup;
 
@@ -47,7 +25,7 @@ public class ECSClient {
             new LogSetup(LOG_FILE, Level.OFF);
             ExecutionContext.setExecutionEnvironmentSystemPropertiesFromArgs(args);
             EcsExecutionContext.setConfigurationFilePath(args[0]);
-            
+
             Injector injector = Guice.createInjector(new EcsClientModule());
             Client ecsClient = injector.getInstance(Client.class);
             ecsClient.run();
