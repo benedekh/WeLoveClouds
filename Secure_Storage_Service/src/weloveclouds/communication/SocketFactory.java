af98e98f554a8a74b907f585ba108901b434ae66
HB <s3529366@student.rmit.edu.au>
2016-12-30 08:06:27 +0100

    Small commit so that I can push

--- Secure_Storage_Service/src/weloveclouds/communication/SocketFactory.java
+++ Secure_Storage_Service/src/weloveclouds/communication/SocketFactory.java
@@ -3,6 +3,7 @@ package weloveclouds.communication;
 import java.io.IOException;
 import java.net.Socket;
 import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
 
 import org.apache.log4j.Logger;
 
@@ -18,6 +19,9 @@ public class SocketFactory {
 
     private static final Logger LOGGER = Logger.getLogger(SocketFactory.class);
 
+    /*This won't work, I need to find out how to get a SSLSocketFactory from an SSLContext in here.*/
+    private final SSLSocketFactory sslSocketFactory = (SSLSocketFactory) SSLSocketFactory.getDefault();
+    
     /**
      * Creates a raw TCP Socket or an SSLSocket using server connection information
      * ({@link ServerConnectionInfo#getIpAddress()} and {@link ServerConnectionInfo#getPort()}).
@@ -31,7 +35,7 @@ public class SocketFactory {
     
     public SSLSocket createSSLSocketFromInfo(ServerConnectionInfo connectionInfo) throws IOException{
         LOGGER.debug(StringUtils.join(" ", "Creating SSL socket for", connectionInfo));
-        return null;
+        return (SSLSocket)sslSocketFactory.createSocket(connectionInfo.getIpAddress(), connectionInfo.getPort());
         //TODO; have this actually return an ssl socket
     }
 }
