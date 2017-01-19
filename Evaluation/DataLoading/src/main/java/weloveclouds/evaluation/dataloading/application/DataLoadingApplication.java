e18a9d0c1682c75f1f97baea124a4cd1c21ed986
benedekh <benedekh12@gmail.com>
2017-01-20 00:08:17 +0100

    * KVClient name is configurable in the data loading project

--- Evaluation/DataLoading/src/main/java/weloveclouds/evaluation/dataloading/application/DataLoadingApplication.java
+++ Evaluation/DataLoading/src/main/java/weloveclouds/evaluation/dataloading/application/DataLoadingApplication.java
@@ -11,6 +11,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.core.LoggerContext;
 
+import app_kvClient.KVClient;
 import weloveclouds.commons.context.ExecutionContext;
 import weloveclouds.evaluation.dataloading.connection.ClientConnection;
 import weloveclouds.evaluation.dataloading.connection.ClientConnectionFactory;
@@ -26,11 +27,12 @@ public class DataLoadingApplication {
 
     private static final Logger LOGGER = LogManager.getLogger(DataLoadingApplication.class);
 
-    private static final int NUMBER_OF_CLI_ARGUMENTS = 4;
+    private static final int NUMBER_OF_CLI_ARGUMENTS = 5;
     private static final int INPUT_CSV_FOLDER_PATH_INDEX = 0;
-    private static final int SERVER_IP_ADDRESS_INDEX = 1;
-    private static final int SERVER_PORT_INDEX = 2;
-    private static final int OPERATION_INDEX = 3;
+    private static final int OPERATION_INDEX = 1;
+    private static final int CLIENT_NAME_INDEX = 2;
+    private static final int SERVER_IP_ADDRESS_INDEX = 3;
+    private static final int SERVER_PORT_INDEX = 4;
 
     static {
         initializeRootLogger();
@@ -53,12 +55,13 @@ public class DataLoadingApplication {
                     String serverIp = args[SERVER_IP_ADDRESS_INDEX];
                     int serverPort = Integer.valueOf(args[SERVER_PORT_INDEX]);
                     Operation operation = Operation.valueOf(args[OPERATION_INDEX]);
-                    
+
                     ClientConnection client =
                             ClientConnectionFactory.createDefaultClient(serverIp, serverPort);
                     CSVFolderTraverser traverser = new CSVFolderTraverser(client);
 
                     ExecutionContext.getExecutionEnvironmentFromArgs(args);
+                    KVClient.CLIENT_NAME = args[CLIENT_NAME_INDEX];
                     traverser.traverseFolder(csvFolderPath, operation);
                 }
             } catch (InvalidPathException ex) {
@@ -66,7 +69,7 @@ public class DataLoadingApplication {
             }
         } else {
             LOGGER.error(
-                    "Required arguments: <input_csv_folder_path> <server_ip_address> <server_port>");
+                    "Required arguments: <input_csv_folder_path> <operation: put/get/delete> <client_name> <server_ip_address> <server_port>");
         }
     }
 
