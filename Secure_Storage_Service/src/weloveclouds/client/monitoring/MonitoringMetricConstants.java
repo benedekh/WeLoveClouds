9b3b82b6f1ec0e1169597bd3f173af5e634c04c1
Benoit Anctil-Robitaille <benoit.robitaille@hotmail.ca>
2017-01-21 20:27:18 +0100

    + Corrected bad refactor of IntelliJ

--- Secure_Storage_Service/src/weloveclouds/client/monitoring/MonitoringMetricConstants.java
+++ Secure_Storage_Service/src/weloveclouds/client/monitoring/MonitoringMetricConstants.java
@@ -6,8 +6,8 @@ package weloveclouds.client.monitoring;
  * @author Benedek
  */
 public class MonitoringMetricConstants {
-    public static final String PUT_COMMAND_NAME = "registerPut";
-    public static final String GET_COMMAND_NAME = "registerGet";
+    public static final String PUT_COMMAND_NAME = "put";
+    public static final String GET_COMMAND_NAME = "get";
 
     public static final String LATENCY = "latency";
 }
